2ef69217b21476217c2c0dd7a93e6c803bfa80bc
Sara Zhou <sara@Saras-MacBook-Air.local>
2024-03-09 16:49:25 +1100

    add js code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 // Access toggle switch HTML element
 const themeSwitcher = document.querySelector('#theme-switcher');
-console.log(themeSwitcher);
+console.log(themeSwitcher.textContent);
 const container = document.querySelector('.container');
 
 // Set default mode to dark
@@ -11,11 +11,25 @@ themeSwitcher.addEventListener('click', function () {
   // If mode is dark, apply light background
   if (mode === 'dark') {
     mode = 'light';
+    localStorage.setItem('mode', mode);
     container.setAttribute('class', 'light');
   }
   // If mode is light, apply dark background
   else {
     mode = 'dark';
+    localStorage.setItem('mode', mode);
     container.setAttribute('class', 'dark');
   }
 });
+
+
+//Render last saved mode option
+function renderLastMode(){
+  const initialMode = localStorage.getItem('mode');
+  if(initialMode !== null){
+    container.setAttribute('class',initialMode);
+  };
+
+};
+renderLastMode();
+
